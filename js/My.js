772f1ad40ddd8e118177e71d4de5d4d5fb74becb
Filip Dimitrovski <filip@ifkreativa.com>
2014-10-27 12:43:18 +0100

    v20

--- js/My.js
+++ js/My.js
@@ -70,8 +70,8 @@ function resize() {
     var h = jQuery(window).height();
     var w = jQuery(window).width();
     jQuery("#HomeContainer").css({ "width": w, "height": h });
-    jQuery("#select-choice-0-section-Day-button").css({ "width": "8%", "width": "8vh" });
-    jQuery("#select-choice-0-section-Day").css({ "width": "8%", "width": "8vh" });
+    jQuery("#select-choice-0-section-Day-button").css({ "height": "8% !important", "width": "8vh !important" });
+    jQuery("#select-choice-0-section-Day").css({ "height": "8% !important", "width": "8vh !important" });
 }
 
 function clearContainers() {
