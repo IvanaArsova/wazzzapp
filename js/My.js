80e20a3d4fcb45bca8e8aa7d8f0ceae7a18fe540
Filip Dimitrovski <filip@ifkreativa.com>
2014-10-27 13:58:38 +0100

    v23

--- js/My.js
+++ js/My.js
@@ -84,7 +84,7 @@ function resize() {
     jQuery("#SettingsContainer").css({ "width": w, "height": h });
     jQuery("#SettingsContainer .lang").css({ "width": w, "height": h / 2 });
 
-    jQuery("#homeHeader").css({ "font-size": h/10 - 4 + "px" })
+    jQuery(".homeHeader").css({ "font-size": h/25 + "px" })
 
 }
 
